5e4c7606ec8741993e212b3f392c42ed0300dcbd
dwpeng <dwpeng1122@foxmail.com>
2024-08-27 09:41:15 +0800

    update test script

--- example/psa/example_psa.sh
+++ example/psa/example_psa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ##Pairwise sequence alignment##
-../../TSTA_psa -M 2 -X -3 -E -2 -O -4 -T 30 -S 30 seq/seqa1.fa seq/seqb1.fa > result.txt
+../../TSTA_psa -M 2 -X -3 -E -2 -O -4 -T 30 -W 30 -1 seq/seqa1.fa -2 seq/seqb1.fa > result.txt
 echo done!
